81a0ce06dd27d43d0996552855d6aecc25fec0a0
helder <helder@5869f155-5c2e-0410-a343-b3218e66c8fe>
2007-05-28 05:20:13 +0000

    sample test for the FireWatirGen module

---
+++ trunk/module_test.rb
@@ -0,0 +1,8 @@
+require 'firewatirgen'
+require 'rubygems'
+require 'firewatir'
+include FireWatir
+include FireWatirGen
+@ff = Firefox.new
+@ff.goto('http://www.voegol.com.br')
+element_by_least_restrictive_xpath('/HTML/BODY/TABLE[1]/TBODY[1]/TR[1]/TD[1]/TABLE[1]/TBODY[1]/TR[1]/TD[1]/DIV[1]/TABLE[1]/TBODY[1]/TR[1]/TD[1]/A')
