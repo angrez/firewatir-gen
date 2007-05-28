c7cf9d5538f11238c0d5097cea666113d5096228
helder <helder@5869f155-5c2e-0410-a343-b3218e66c8fe>
2007-05-28 05:18:08 +0000

    added readable debugging prints

--- firewatirgen.rb
+++ firewatirgen.rb
@@ -1,9 +1,13 @@
 module FireWatirGen
   # xpath must be rooted (starts with /bla)
   def element_by_least_restrictive_xpath(xpath)
+    puts '## full xpath:'
+    puts xpath
     last_element = []
     xpath.split(/\//).reverse.each do |element|
       ele = @ff.elements_by_xpath('//' + last_element.push(element).reverse.join('/'))
+      puts '-- current xpath: //' + last_element.reverse.join('/')
+      puts "-- number of elements: #{ele.length}"
       if ele.length == 1
         puts '## least restrictive xpath that guarantees uniqueness:'
         puts '//'+last_element.reverse.join('/')
